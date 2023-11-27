1b30fa0b7401882fc34b2cfc226c61459b1d09b8
Carlitos <116768102+carlitoswolf@users.noreply.github.com>
2023-11-26 22:59:46 -0600

    Initial Commit

---
+++ models/user.js
@@ -0,0 +1,186 @@
+const db = require('../config/config');
+const bcrypt = require('bcryptjs');
+const nodeMailer = require('nodemailer');
+var code = 0;
+
+const User = {};
+
+User.create = async (user, result) => {
+
+    const hash = await bcrypt.hash(user.password, 10);
+
+    //sendMail2(user.email);
+
+    const sql = `
+        INSERT INTO
+            usuarios(   
+                name,
+                lastname,
+                phone,
+                email,
+                password,
+                photo,
+                code
+            )
+        VALUES(?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    db.query
+        (
+            sql,
+            [
+                user.name,
+                user.lastname,
+                user.phone,
+                user.email,
+                hash,
+                user.image,
+                "here!"
+            ],
+            (err, res) => {
+                if (err) {
+                    console.log('Error:', err);
+                    result(err, null);
+                }
+                else {
+                    console.log('Id del nuevo usuario:', res.insertId);
+                    result(null, res.insertId);
+                }
+            }
+        )
+
+}
+
+User.findById = (id, result) => {
+
+    const sql = `
+    SELECT
+        CONVERT(id, char) AS id,
+        name,
+        lastname,
+        phone,
+        email,
+        password,
+        photo
+    FROM   
+        usuarios
+    WHERE
+        id = ?`;
+
+    db.query(
+        sql,
+        [id],
+        (err, user) => {
+            if (err) {
+                console.log('Error:', err);
+                result(err, null);
+            }
+            else {
+                console.log('Usuario obtenido:', user[0]);
+                result(null, user[0]);
+            }
+        }
+    )
+
+}
+
+User.findByEmail = (email, result) => {
+
+    const sql = `
+    SELECT
+        CONVERT(id, char) AS id,
+        name,
+        lastname,
+        phone,
+        email,
+        password,
+        photo
+    FROM   
+        usuarios
+    WHERE
+        email = ?
+    `;
+
+    db.query(
+        sql,
+        [email],
+        (err, user) => {
+            if (err) {
+                console.log('Error:', err);
+                result(err, null);
+            }
+            else {
+                console.log('Usuario obtenido:', user[0]);
+                result(null, user[0]);
+            }
+        }
+    )
+
+}
+
+
+User.findByCode = (code, result) => {
+
+    const sql = `
+    SELECT 
+        code 
+    FROM 
+        usuarios 
+    WHERE code = ?
+    `;
+
+    db.query(
+        sql,
+        [code],
+        (err, user) => {
+            if (err) {
+                console.log('Error:', err);
+                result(err, user[0]);
+            }
+            else {
+                console.log('Codigo obtenido:', user[0]);
+                result(null, user[0]);
+            }
+        }
+    )
+
+}
+
+
+module.exports = User;
+
+sendMail2 = async (email) => {
+
+    const config = {
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: 'turciosjimenez@gmail.com',
+            pass: 'jizy pnij vrgg hrlj'
+        }
+    }
+
+    code = generateCodeAccess();
+    
+    console.log(code);
+
+    console.log(email);
+
+    const mensaje = {
+        from: 'turciosjimenez@gmail.com',
+        to: `${email}`,
+        subject: 'Correo De Verificacion de Usuario',
+        text: `Este es su codigo de acceso ${code}`
+    }
+
+    const transport = nodeMailer.createTransport(config);
+
+    const info = await transport.sendMail(mensaje);
+
+    console.log(info);
+
+}
+
+function generateCodeAccess() {
+    return Math.floor(10000 + Math.random() * 90000);
+}
---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const usersController = require('../controllers/userController');
+const passport = require('passport');
+
+module.exports = (app, upload) => {
+
+    // GET -> OBTENER DATOS
+    // POST -> ALMACENAR DATOS
+    // PUT -> ACTUALIZAR DATOS
+    // DELETE -> ELIMINAR DATOS
+
+    app.post('/api/users/create', usersController.register);
+    app.post('/api/users/login', usersController.login);
+    app.post('/api/users/createWithImage', upload.array('image', 1), usersController.registerWithImage);
+    
+    app.get('/api/users/findByCode/:code', passport.authenticate('jwt', { session: false }), usersController.findByCode);
+    
+}   
