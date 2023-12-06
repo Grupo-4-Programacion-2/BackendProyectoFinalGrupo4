23acf2c811166fb4300993bdddf1d6700a7bfd5f
Carlitos <116768102+carlitoswolf@users.noreply.github.com>
2023-12-06 15:30:42 -0600

    Commit

--- routes/recordRoutes.js
+++ routes/recordRoutes.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 
 module.exports = (app, upload)=>{
     app.post('/api/records/create', passport.authenticate('jwt', { session: false }), upload.array('image', 1), recordController.registerTasks);
-    app.get('/api/users/getAll/:userId', recordController.getAllData);
+    app.get('/api/users/getAll/:userId/:status', recordController.getAllData);
     app.delete('/api/records/delete/:id', passport.authenticate('jwt', { session: false }), recordController.deleteTask);
     
     app.put('/api/records/update', passport.authenticate('jwt', { session: false }), upload.array('image', 1), recordController.updateWithImage);
